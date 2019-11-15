a435b230d3b1e288004764f846ab6320d9e427d4
kearstinfindley <kearstin.findley@selu.edu>
2019-11-15 19:31:34 +0000

    Answer to questions

---
+++ R/MS_Student_3_Questions_Project_3.R
@@ -0,0 +1,7 @@
+- What else do you need covered in this class? 
+  # Maybe some basics on how to set up spreadsheets/data to put into R (formating, what basic characters are considered "special characters" in R that might cause problems). 
+  # Also, when I get errors I try googling my issues and it never really seems to help me very much. Is there a better way to search errors other than just copy and pasting the error code you get, or are there specific websites that have user friendly tips?
+  - Is anyone else in your lab doing similar analyses?
+  # Everyone in our lab is using R to do different things, but we all generally need to make a map at some point, so the mapping we have learned how to do in leaflet would be a useful skill amongst us all. 
+  - Do you think in the future, there will be other students working on your question in your lab?
+  #I don't know if anyone will be working on similar research in the future, but the things I have been using R for in my dataset are things that someone would most definitely find useful working with their data (mapping, converting coordinates, plotting abiotic data, etc.).
