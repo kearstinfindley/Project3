c201f30314511d4963806ffd14f3192b4eefaf71
kearstinfindley <kearstin.findley@selu.edu>
2019-11-26 16:11:10 +0000

    Package

--- R/converting_dms_to_degrees.R
+++ R/converting_dms_to_degrees.R
@@ -1,37 +1,11 @@
-These functions convert latitude and longitude coordinates in degree minute seconds format to decimal degrees format.
-This is super helpful because the gps I use in the field gives you coordinates in dms, and then I convert
-to decimal degrees by hand later. 
-
-Instructions for Latitude conversion: 
-   1) install packages below (tidyverse) and load the librarys below (tidyverse, tidyr, dplyr)
-   2) upload your dataframe in a csv format
-   3) set your dataframe to a variable (project_data_coord)
-   4) create the function referencing your dataframe: 
-      5) separate your values in the latitude column by (degree minute seconds)
-      6) combine the d,m,s in a new column
-      7) get rid of any NA values
-      8) convert from degree minute seconds to decimal degrees
-      9) return the function
-   10) apply the function to your data to view the converted values
-
-Instructions for longitude conversion function:
-   1) follow steps 1-4 above for setting up your function then:
-      2) separate degree minute seconds in your longitude column and convert the values to numeric values
-      3) drop NA values
-      4) divide the minutes values by 60
-      5) divide the seconds value by 3600
-      6) add the deg minute seconds values in a new column to convert from dms to dd
-      9) return the function
-   10) apply the function to your data to view the converted values
-
-
-install.packages("tidyverse")
-library(tidyverse)
-library(dplyr)
-library(tidyr)
-
-
-project_data_coord <- read.csv("/cloud/project/data/R_project_3_data_coord.csv", stringsAsFactors = FALSE)
+#' Convert degree minute second coordinate values to decimal degree values. 
+#'
+#' Convert degree minute second coordinate values to decimal degree values.
+#' 
+#' @param project_data_coord A dataframe containing the columns Latitude and Longitude that have
+#' coordinates in the degree minute second format to be converted to the decimal degree format.
+#' @return splt A new column with the converted decimal degree coordinate values.
+#' @export final_latitude and final_longitude
 
 
  
@@ -43,8 +17,7 @@ project_data_coord <- read.csv("/cloud/project/data/R_project_3_data_coord.csv",
       mutate(deg = dms2deg(FinalDMS, sep=","))
       return(splt)}
    
- lat$deg
- 
+
  
    
    finalLong <- function(project_data_coord){
@@ -56,5 +29,5 @@ project_data_coord <- read.csv("/cloud/project/data/R_project_3_data_coord.csv",
        mutate(finalDMS = D + deg_minute + deg_second)
      return(splt)}
 
-  dat$finalDMS 
+ 
    
--- R/mapping_species.R
+++ R/mapping_species.R
@@ -8,37 +8,6 @@
 #' @export map_genera
 
 
-This function maps the localites I have sampled thus far for each one of my
-species. The localities are colored by genera, and the genera included only have 
-one representative species. 
-
-
-Instructions to map genera:
-1) install needed packages below (tidyverse and leaflet)
-2) load needed librarys below (tidyverse, dplyr, and leaflet)
-3) upload your data frame in a csv format
-4) set your data frame to a variable (project_data or map)
-5) create a color palate using levs and factpal to color by genera
-6) set up function to map by genera referencing your dataframe:
-        7) group by your desired column (Genus)
-        8) set up a leaflet map
-        9) add your background map (addProviderTiles)
-        10) add your makers by plotting your coordinate columns (decimalLatiude/decimalLongitude) and coloring them by the Genus column
-        11) return the map
-12) plot the map with your data
-
-install.packages("tidyverse")
-library(tidyverse)
-library(dplyr)
-install.packages("leaflet")
-library(leaflet)
-
-project_data <- read_csv("/cloud/project/data/R_project_3_data.csv")
-
-map <- project_data
-
-levs <- factor(project_data$Genus)
-factpal <- colorFactor(topo.colors(5), levs)
 
 map_genera <- function(map) {
        genera <- map %>% 
@@ -48,6 +17,6 @@ map_genera <- function(map) {
        addCircleMarkers(~decimalLongitude, ~decimalLatitude, color = ~factpal(Genus), stroke = FALSE, fillOpacity = 0.5) 
        return(genera)}
 
-map_genera(project_data)
+
 
 
